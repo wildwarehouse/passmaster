4becea32808d745969a17eb55e8152cb4bca8b20
Emory Merryman <emory.merryman@gmail.com>
2017-03-18 09:32:58 +0000

    Basic Implementation

--- README.md
+++ README.md
@@ -17,3 +17,41 @@
 -->
 
 # passmaster
+
+## Usage
+
+```
+export GPG_KEY_VOLUME &&
+    export GPG_KEY_ID &&
+    export GIT_USER_NAME &&
+    export GIT_USER_EMAIL &&
+    export GPG_PRIVATE_TOKEN &&
+    docker \
+        run \
+        --interactive \
+        --tty \
+        --rm \
+        --volume ${GPG_KEY_VOLUME}:/srv:ro \
+        --env GPG_KEY_ID \
+        --env GIT_USER_NAME \
+        --env GIT_USER_EMAIL \
+        --env GPG_PRIVATE_TOKEN \
+        wildwarehouse/passmaster:0.0.0
+```
+
+where
+
+1. GPG_KEY_VOLUME is a docker volume with the following files:  secret1.key, secret2.key, owner1.trust, and owner2.trust.
+   1. secret1.key is the result of `gpg --export-secret-keys --output secret1.key`
+   2. secret2.key is the result of `gpg2 --export-secret-keys --output secret2.key`
+   3. owner1.trust is the result of `gpg --export-ownertrust -output owner1.trust`
+   4. owner2.trust is the result of `gpg2 --export-ownertrust ---output owner2.trust`
+   5. it is believe that owner1.trust is the same as owner2.trust
+2. GPG_KEY_ID is the result of `gpg --list-keys`
+3. GIT_USER_NAME is just your name / or whatever you want to use for git.
+4. GIT_USER_EMAIL is just your email / or whatever you want to use for git.
+5. GPG_PRIVATE_TOKEN should be github private token.
+   1. Login in to github.
+   2. Visit https://github.com/settings/tokens
+   3. Generate a new token.  Be sure it has enough privileges.  (I just grant them all.)
+   4. Copy the token.  If you forget the token you must regenerate it.
---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Copyright © (C) 2017 Emory Merryman <emory.merryman@deciphernow.com>
+#   This file is part of passmaster.
+#
+#   passmaster is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   passmaster is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with passmaster.  If not, see <http://www.gnu.org/licenses/>.
+
+gpg --batch --import /srv/secret1.key &&
+    gpg --batch --import-ownertrust /srv/owner1.trust &&
+    gpg2 --batch --import /srv/secret2.key &&
+    gpg2 --batch --import-ownertrust /srv/owner2.trust &&
+    pass init ${GPG_KEY_ID} &&
+    pass git init &&
+    pass git config user.name "${GIT_USER_NAME}" &&
+    pass git config user.email "${GIT_USER_EMAIL}" &&
+    pass git remote add origin git@github.com:desertedscorpion/passwordstore.git &&
+    ssh-keygen -f /root/.ssh/id_rsa -P "" -C "" &&
+    (cat <<EOF
+{
+    "title": "generated",
+    "key": "$(cat /root/.ssh/id_rsa.pub)"
+}
+EOF
+    ) | curl --data @- "https://api.github.com/user/keys?access_token=${GPG_PRIVATE_TOKEN}" &&
+    cp /opt/docker/config /root/.ssh &&
+    chmod 0600 /root/.ssh/config &&
+    cp /opt/docker/known_hosts /root/.ssh &&
+    chmod 0600 /root/.ssh/known_hosts &&
+    pass git fetch origin master &&
+    pass git checkout master &&
+    bash
